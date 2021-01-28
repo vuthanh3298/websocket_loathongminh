23a1ee21ddfe9f885138952cd04da97180312304
vuthanh3298 <vuthanh3298@gmail.com>
2021-01-28 12:58:55 +0700

    init

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+const WebSocket = require("ws");
+const path = require("path");
+
+const wss = new WebSocket.Server({ server: server });
+
+wss.on("connection", function connection(ws) {
+  console.log("A new client Connected!");
+});
+
+app.set("views", path.join(__dirname, "views"));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("client.html");
+});
+
+app.get("/light", (req, res) => {
+  console.log(req.query.data);
+  wss.clients.forEach(function each(client) {
+    client.send(req.query.data);
+  });
+  res.end();
+});
+
+server.listen(3000, () => console.log(`Lisening on port :3000`));
